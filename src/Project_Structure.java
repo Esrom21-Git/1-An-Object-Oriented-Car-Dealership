7d44198bc7fd3f096f57912af9de794c97712335
Esrom Tekle <kidaneesrom34@gmail.com>
2025-05-09 14:26:27 -0700

    First project done

---
+++ src/Project_Structure.java
@@ -0,0 +1,19 @@
+//Car Dealership Project Structure
+
+//```
+//src/
+     //   ├── main/
+      //  │   └── java/
+    //    │       └── com/
+      //  │           └── dealership/
+      //  │               ├── model/
+       // │               │   ├── Vehicle.java
+│        //       │   └── Dealership.java
+│         //      ├── data/
+        │  //             │   └── DealershipFileManager.java
+│           //    ├── ui/
+        │     //          │   └── UserInterface.java
+│             //  └── Program.java
+├─//─ resources/
+      //  │   └── inventory.csv
+```
